ce43b6192ee93f8a15bd7f6642074925e8cee311
eduardo secco <secco654@gmail.com>
2026-06-13 14:48:15 -0300

    testes-qualidade

--- init.sql
+++ init.sql
@@ -29,3 +29,6 @@ INSERT INTO receita (nome, descricao, data_registro, custo, tipo_receita) VALUES
 
 INSERT INTO usuario (nome, login, senha, situacao) VALUES
 ('Administrador', 'admin', 'admin123', 'a');
+
+CREATE TABLE IF NOT EXISTS categoria 
+(id SERIAL, tipo VARCHAR, nome VARCHAR);
